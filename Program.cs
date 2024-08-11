4b809a01b1b8817fd624a3e388e483bde7842077
JmihPojiloy <mr.d.pod@mail.ru>
2024-08-11 18:01:50 +0300

    delete bot token

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace bot
 {
     class Program
     {
-        private static readonly string BotToken = "7237204335:AAEBmcnXd8mgDlT_tO7HV0sETGgAfD-UG40";
+        private static readonly string BotToken = "";
         private static readonly string ApiBaseAddress = "http://localhost:5124/Lead";
 
         static void Main(string[] args)
